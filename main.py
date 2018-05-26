e14b73c4648445d8bc4ed8bf131c0d220592ec3f
Alvin Shi <alvin_pisces@hotmail.com>
2018-05-27 06:31:02 +1000

    Add print template, print tax brackets, tax report, main

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # Author: Alvin Shi
 # Date: 27/5/2018
 
-# Tax brackets and rates as of 27/05/18
-TAX_THRESHOLDS = (18200, 37000, 87000, 18000)  # Tax Brackets
+LAST_UPDATE = "27/05/2018"
+TAX_THRESHOLDS = (18200, 37000, 87000, 180000)  # Tax Brackets
 RATES = (0.190,  0.325, 0.370, 0.450) # Tax Rate
+
 
 def calculate_tax(remaining_salary, tax_threshold, rate, tax=0):
 	taxable_income = remaining_salary - tax_threshold
@@ -22,9 +23,37 @@ def get_salary():
 		except ValueError:
 			print("Please enter a valid number without special characters except the decimal point")
 
+def print_table(contents):
+	# Padding values arrived at through trial and error
+	print("{:.15}{:.15}{:.15}".format(*contents))
+
+
+def print_brackets(brackets):
+	print("")
+	print("CURRENT TAX BRACKET")
+	print("last updated: " + LAST_UPDATE)
+	print_table(["BRACKET", "THRESHOLD", "RATE"])
+	for number in range (1, len(brackets)+1):
+		threshold = brackets[number-1][0]
+		rate = brackets[number-1][1]*100
+		print_table([str(number), "$"+str(threshold), str(rate)+"%"])
+	print("")
+
+def tax_report(salary, tax):
+	print("")
+	print_table(["TIME INTERVAL", "BEFORE", "AFTER"])
+	print_table(["YEARLY", "$" + str(float(salary)), "$" + str(float(salary-tax))])
 
 def main():
 	salary = get_salary()
+	initial_salary = salary
+	tax = 0
+	brackets = list(zip(TAX_THRESHOLDS, RATES))
+
+	print_brackets(brackets)
+	for (threshold, rate) in reversed(brackets):
+		(salary, tax) = calculate_tax(salary, threshold, rate, tax)
+	tax_report(initial_salary, tax)
 
 if __name__ == '__main__':
 	main()
