bd7686250602a8484f0c042fb1df1896f13ee6fb
Alvin Shi <alvin_pisces@hotmail.com>
2019-03-29 01:12:38 +1100

    Add on threshold test

--- tests/test_tax_calculator.py
+++ tests/test_tax_calculator.py
@@ -8,22 +8,29 @@ class TestTaxCalculator(unittest.TestCase):
 
     def test_below_threshold(self):
         income = 100
-        rates = [{'threshold': 1000, 'rate': 0.1}]
-        (after_tax, taxed) = calculate_tax(income, rates)
+        brackets = [{'threshold': 1000, 'rate': 0.1}]
+        (after_tax, taxed) = calculate_tax(income, brackets)
         self.assertEqual(income, after_tax)
         self.assertEqual(0, taxed)
 
+    def test_on_threshold(self):
+        income = 1500
+        brackets = [{'threshold': 1000, 'rate': 0.1}, {'threshold': 1500, 'rate': 0.2}]
+        (after_tax, taxed) = calculate_tax(income, brackets)
+        self.assertEqual(1450, after_tax)
+        self.assertEqual(50, taxed)
+
     def test_above_first_threshold(self):
         income = 2000
-        rates = [{'threshold': 1000, 'rate': 0.1}]
-        (after_tax, taxed) = calculate_tax(income, rates)
+        brackets = [{'threshold': 1000, 'rate': 0.1}]
+        (after_tax, taxed) = calculate_tax(income, brackets)
         self.assertEqual(1900, after_tax)
         self.assertEqual(100, taxed)
 
     def test_above_multiple_thresholds(self):
         income = 2000
-        rates = [{'threshold': 1000, 'rate': 0.1}, {'threshold': 1500, 'rate': 0.2}]
-        (after_tax, taxed) = calculate_tax(income, rates)
+        brackets = [{'threshold': 1000, 'rate': 0.1}, {'threshold': 1500, 'rate': 0.2}]
+        (after_tax, taxed) = calculate_tax(income, brackets)
         self.assertEqual(1850, after_tax)
         self.assertEqual(150, taxed)
 
