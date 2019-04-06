5dc0dff759037313d823b567ad7f55362b7f1bed
Alvin Shi <alvin_pisces@hotmail.com>
2019-04-07 01:37:52 +1100

    Add functions to get different brackets
    - Hides generation of brackets
    - Fix generate_tax error

--- generate_brackets.py
+++ generate_brackets.py
@@ -1,3 +1,5 @@
+from tax_brackets_data import *
+
 def generate_brackets(thresholds, rates):
     if len(thresholds) != len(rates):
         return None
@@ -7,3 +9,12 @@ def generate_brackets(thresholds, rates):
     for i in range(len(thresholds)):
         brackets.append({"threshold": thresholds[i], "rate": rates[i]})
     return brackets
+
+def get_income_tax_brackets():
+    return generate_brackets(TAX_THRESHOLDS, TAX_RATES)
+
+def get_hecs_brackets():
+    return generate_brackets(HELP_THRESHOLDS, HELP_RATES)
+
+def get_medicare_levy_brackets():
+    return generate_brackets(MEDICARE_LEVY_THRESHOLD, MEDICARE_LEVY_RATE)
--- main.py
+++ main.py
@@ -7,18 +7,20 @@
 
 from tax_brackets_data import *
 from user_input import get_settings
-from calculate_tax import calculate_tiered_tax, calculate_flat_rate_tax
-from generate_brackets import generate_brackets
+from calculate_tax import \
+    calculate_tiered_tax, calculate_flat_rate_tax
+from generate_brackets import \
+    get_income_tax_brackets, get_hecs_brackets, get_medicare_levy_brackets
 
 def main():
-    tax_brackets = generate_brackets(TAX_THRESHOLDS, TAX_RATES)
-    hecs_brackets = generate_brackets(HELP_THRESHOLDS, HELP_RATES)
-    medicare_brackets = generate_brackets(MEDICARE_LEVY_THRESHOLD, MEDICARE_LEVY_RATE)
+    tax_brackets = get_income_tax_brackets()
+    hecs_brackets = get_hecs_brackets()
+    medicare_brackets = get_medicare_levy_brackets()
     # medicare_surcharge = generate_brackets(MEDICARE_SURCHARGE_THRESHOLD, MEDICARE_SURCHARGE_RATE)
     (income, family_income, has_help) = get_settings()
     # OUTPUT
     # [income, tax, hecs, medicare, after_tax]
-    (after_tax, tax) = calculate_tax(income, tax_brackets)
+    (after_tax, tax) = calculate_tiered_tax(income, tax_brackets)
     (after_hecs, hecs) = calculate_flat_rate_tax(income, hecs_brackets)
     (after_medicare, medicare) = calculate_flat_rate_tax(income, medicare_brackets)
     post_tax = income - tax - hecs - medicare
