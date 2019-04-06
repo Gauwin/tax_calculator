97ddfe6a8e92b996ddd657f190b2cc264767bbdb
Alvin Shi <alvin_pisces@hotmail.com>
2019-04-07 01:16:06 +1100

    Add main function of program

--- main.py
+++ main.py
@@ -10,5 +10,20 @@ from user_input import get_settings
 from calculate_tax import calculate_tiered_tax, calculate_flat_rate_tax
 from generate_brackets import generate_brackets
 
+def main():
+    tax_brackets = generate_brackets(TAX_THRESHOLDS, TAX_RATES)
+    hecs_brackets = generate_brackets(HELP_THRESHOLDS, HELP_RATES)
+    medicare_brackets = generate_brackets(MEDICARE_LEVY_THRESHOLD, MEDICARE_LEVY_RATE)
+    # medicare_surcharge = generate_brackets(MEDICARE_SURCHARGE_THRESHOLD, MEDICARE_SURCHARGE_RATE)
+    (income, family_income, has_help) = get_settings()
+    # OUTPUT
+    # [income, tax, hecs, medicare, after_tax]
+    (after_tax, tax) = calculate_tax(income, tax_brackets)
+    (after_hecs, hecs) = calculate_flat_rate_tax(income, hecs_brackets)
+    (after_medicare, medicare) = calculate_flat_rate_tax(income, medicare_brackets)
+    post_tax = income - tax - hecs - medicare
+    print([income, tax, hecs, medicare, post_tax])
 
 
+if __name__ == '__main__':
+    main()
